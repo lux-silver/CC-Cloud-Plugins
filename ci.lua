c5078f40f5bda5d2c24ad795d662f8a850fbcfa2
Lux_Silver <114696881+lux-silver@users.noreply.github.com>
2026-06-08 02:15:41 -0300

    Add files via upload

--- ci.lua
+++ ci.lua
@@ -1,4 +1,4 @@
-run.shell(wget https://raw.githubusercontent.com/lux-silver/CC-Cloud-Plugins/main/cloud_user)
-run.shell(wget https://raw.githubusercontent.com/lux-silver/CC-Cloud-Plugins/main/cloud)
-run.shell(wget https://raw.githubusercontent.com/lux-silver/CC-Cloud-Plugins/main/plugins/install.lua)
+run.shell("wget https://raw.githubusercontent.com/lux-silver/CC-Cloud-Plugins/main/cloud_user")
+run.shell("wget https://raw.githubusercontent.com/lux-silver/CC-Cloud-Plugins/main/cloud")
+run.shell("wget https://raw.githubusercontent.com/lux-silver/CC-Cloud-Plugins/main/plugins/install.lua")
 run.shell("cloud")
